2e861ce49f43df9337237cf9d294962eb8d4c5ef
Niroshan Vijayarasa <niroshan.vijayarasa@epfl.ch>
2017-11-27 22:54:03 +0100

    add files

--- Project/load_df.py
+++ Project/load_df.py
@@ -33,6 +33,7 @@ if __name__ == '__main__':
 
     take_n = rdd_list.take(n)
     output_path ="/home/vijayara/niro.csv"
+    hdfs_pathname = "niroSource.txt"
 
     df.registerAsTable('df_table')
     sql_request= "SELECT Date,Source,Target,CAMEOCode,NumEvents,NumArts " \
@@ -40,19 +41,27 @@ if __name__ == '__main__':
           "WHERE (Source IN ('AFG','SYR','IRQ','MEX', 'PAK') OR Target IN ('AFG','SYR','IRQ','MEX', 'PAK')) AND " \
           "(CAMEOCode LIKE '18%' OR CAMEOCode LIKE '19%' OR CAMEOCode LIKE '20%' )" \
 
-    sql_request_group_by = "SELECT MonthYear,Target,EventCode,SUM(NumEvents), SUM(NumArts) " \
+    sql_request_group_by = "SELECT SUBSTRING( Date ,1 , 6) AS MonthYear,Target,SUBSTRING(CAMEOCode,1,2) AS EventCode,SUM(NumEvents), SUM(NumArts) " \
                   "FROM df_table " \
                   "WHERE (Target IN ('AFG','SYR','IRQ','MEX', 'PAK')) AND " \
                   "(CAMEOCode LIKE '18%' OR CAMEOCode LIKE '19%' OR CAMEOCode LIKE '20%' ) AND " \
                            "(Date > 20021231 AND Date < 20140101) " \
-                           "GROUP BY ( SUBSTRING( Date ,1 , 6) AS MonthYear ,Target, SUBSTRING(CAMEOCode,1,2) AS EventCode )" \
+                    "GROUP BY SUBSTRING( Date ,1 , 6), Target, SUBSTRING(CAMEOCode,1,2)" \
 
 
+    sql_request_group_by2 = "SELECT SUBSTRING( Date ,1 , 6) AS MonthYear,Source,SUBSTRING(CAMEOCode,1,2) AS EventCode,SUM(NumEvents), SUM(NumArts) " \
+                           "FROM df_table " \
+                           "WHERE (Source IN ('AFG','SYR','IRQ','MEX', 'PAK')) AND " \
+                           "(CAMEOCode LIKE '18%' OR CAMEOCode LIKE '19%' OR CAMEOCode LIKE '20%' ) AND " \
+                           "(Date > 20021231 AND Date < 20140101) " \
+                           "GROUP BY SUBSTRING( Date ,1 , 6), Source, SUBSTRING(CAMEOCode,1,2)"
 
-    df_filter = sqlContext.sql(sql_request_group_by)
+    df_filter = sqlContext.sql(sql_request_group_by2).cache()
+    #df.filter.save()
     df_filter.show()
     #print(df_filter.count())
-    #df_filter.coalesce(1).write.json(output_path)
+    #df.write.save('/target/path/', format='parquet', mode='append')  ## df is an existing DataFrame object.
+    df_filter.write.json(hdfs_pathname)
 
 
     sc.stop()
